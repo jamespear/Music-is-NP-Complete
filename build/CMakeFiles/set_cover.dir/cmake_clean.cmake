2fbadb83cec6971e10cf9829cc7e2a899fd0fcac
James Pearce <jamespearce@berkeley.edu>
2024-03-26 10:14:55 -0400

    init

---
+++ build/CMakeFiles/set_cover.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/set_cover.dir/src/main.cpp.o"
+  "CMakeFiles/set_cover.dir/src/main.cpp.o.d"
+  "set_cover"
+  "set_cover.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/set_cover.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/set_cover.dir/src/main.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/set_cover.dir/src/main.cpp.o: \
+  /Users/jamespearce/sp24/set_cover/src/main.cpp
